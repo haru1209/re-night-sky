fe9450cea7f408938349a997308138284bdd5535
Haru <cutemiffy1209@gmail.com>
2026-05-04 22:46:13 +0800

    feat: add star creation and romance sentence display functionality

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import "./App.css";
 
 function App() {
+  const containerRef = useRef<HTMLDivElement>(null);
   const inputRef = useRef<HTMLInputElement>(null);
   const discriptionContainerRef = useRef<HTMLDivElement>(null);
   const discriptionRef = useRef<HTMLParagraphElement>(null);
@@ -22,6 +23,7 @@ function App() {
 
   const [discriptionText, setDiscriptionText] = useState("");
   const [inputValue, setInputValue] = useState("");
+  const [romanceSentence, setRomanceSentence] = useState("");
 
   const typewriterEffect = (textArg: string, callback: () => void) => {
     let discriptionArr = Array.from(textArg);
@@ -56,6 +58,49 @@ function App() {
         });
         return;
       }
+
+      if (!Number.isInteger(n) || n < 1 || n > 100) {
+        setInputValue("");
+        setDiscriptionText("");
+        inputRowRef.current?.classList.add("hidden");
+        typewriterEffect(outOfRangeDiscriptionText, () => {
+          inputRowRef.current?.classList.remove("hidden");
+          inputRef.current?.focus();
+        });
+        return;
+      }
+      discriptionContainerRef.current?.classList.add("transparent");
+      createStars(n);
+    }
+  };
+
+  const createStars = (count: number) => {
+    for (let i = 0; i < count; i++) {
+      let starElement = document.createElement("span");
+
+      starElement.className = "star";
+      starElement.textContent = "*";
+      starElement.style.left = Math.random() * 100 + "%";
+      starElement.style.top = Math.random() * 100 + "%";
+      setTimeout(() => {
+        containerRef.current?.appendChild(starElement);
+        if (i === count - 1) {
+          sentenceRef.current?.classList.add("show");
+          showRomnceSentence(romanceSentenceText);
+          retryRef.current?.classList.remove("btn-disabled");
+        }
+      }, 100 * i);
+    }
+  };
+
+  const showRomnceSentence = (textArg: string) => {
+    const romanceSentenceArr = Array.from(textArg);
+    for (let i = 0; i < romanceSentenceArr.length; i++) {
+      let text = document.createElement("span");
+      text.innerHTML = romanceSentenceArr[i];
+      setTimeout(() => {
+        setRomanceSentence((prev) => prev + text.innerHTML);
+      }, 50 * i);
     }
   };
 
@@ -66,34 +111,63 @@ function App() {
     });
   }, []);
 
+  const resetScene = () => {
+    const stars = document.querySelectorAll(".star");
+    stars.forEach((star) => {
+      star.remove();
+    });
+
+    sentenceRef.current?.classList.remove("show");
+    discriptionContainerRef.current?.classList.remove("transparent");
+    retryRef.current?.classList.add("btn-disabled");
+
+    setInputValue("");
+
+    setDiscriptionText("");
+    inputRowRef.current?.classList.add("hidden");
+    typewriterEffect(discriptionText, () => {
+      inputRowRef.current?.classList.remove("hidden");
+      inputRef.current?.focus();
+    });
+    setRomanceSentence("");
+  };
+
   return (
     <>
-      <div className="discription-container" ref={discriptionContainerRef}>
-        <p className="discription" ref={discriptionRef}>
-          {discriptionText}
+      <div className="container" ref={containerRef}>
+        <div className="discription-container" ref={discriptionContainerRef}>
+          <p className="discription" ref={discriptionRef}>
+            {discriptionText}
+          </p>
+          <div className="input-row hidden" ref={inputRowRef}>
+            <span className="input-hint" ref={inputHintRef}>
+              &gt;
+            </span>
+            <input
+              className="input"
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+              onKeyDown={handleInputKeyDown}
+              type="text"
+            />
+          </div>
+        </div>
+        <div className="sentence" ref={sentenceRef}>
+          <p className="romance-sentence" ref={romanceSentenceRef}>
+            {romanceSentence}
+          </p>
+          <div
+            className={`retry  ${romanceSentence ? "" : "btn-disabled"}`}
+            onClick={resetScene}
+            ref={retryRef}
+          >
+            Retry
+          </div>
+        </div>
+        <p className="author" ref={authorRef}>
+          By Haru 2026
         </p>
-        <div className="input-row hidden" ref={inputRowRef}>
-          <span className="input-hint" ref={inputHintRef}>
-            &gt;
-          </span>
-          <input
-            className="input"
-            value={inputValue}
-            onChange={(e) => setInputValue(e.target.value)}
-            onKeyDown={handleInputKeyDown}
-            type="text"
-          />
-        </div>
       </div>
-      <div className="sentence" ref={sentenceRef}>
-        <p className="romance-sentence" ref={romanceSentenceRef}></p>
-        <div className="retry btn-disabled" ref={retryRef}>
-          Retry
-        </div>
-      </div>
-      <p className="author" ref={authorRef}>
-        By Haru 2026
-      </p>
     </>
   );
 }
